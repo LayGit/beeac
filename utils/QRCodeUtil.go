30aad57b342f858fa6c46236a5c4559ca0666126
Lay <jserlay@gmail.com>
2016-12-20 22:45:25 +0800

    普通 base64

--- utils/QRCodeUtil.go
+++ utils/QRCodeUtil.go
@@ -1,12 +1,15 @@
 package utils
 
-import qrcode "github.com/skip2/go-qrcode"
+import (
+  qrcode "github.com/skip2/go-qrcode"
+  "encoding/base64"
+)
 
 func QRCodeEncode(url string, size int) string {
   byt, err := qrcode.Encode(url, qrcode.Medium, size)
   if err != nil {
     return ""
   } else {
-    return "data:image/png;base64," + Base64Encode(byt)
+    return "data:image/png;base64," + base64.StdEncoding.EncodeToString(byt)
   }
 }
